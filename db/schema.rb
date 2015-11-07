16c781811e57936745306e951c237eb41e98f3e6
Jorge Naranjo <j.naranjojr@gmail.com>
2015-11-07 17:34:09 -0500

    Add Postgres as default Database

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151107213916) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
